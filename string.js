16759114b4dbbc5bc1b27dcf3e716dd8c09c5dcc
kalpesh-dhola <ad.kalpeshdhola@gmail.com>
2025-08-21 10:44:43 +0000

    make string.js file

---
+++ string.js
@@ -0,0 +1,64 @@
+let name = "kalpesh Dhola";
+// console.log(name.length) // give arrays length
+
+let name2 = "kalpesh Dhola";
+// console.log(name2.charAt(0)); // give the alphabet at that position
+
+let name3 = "kalpesh dhola";
+// console.log(name3.at(-1)); // supports negative index
+
+let name4 = "kalpesh dhola";
+// console.log(name4.indexOf("l")); // give the index of element
+
+let name5 = "kalpesh dhola";
+// console.log(name5.lastIndexOf("l")); // give the index of element on the last
+
+let name6 = "kalpesh dhola";
+// console.log(name6.includes("kalpesh"));  // give ture and false
+
+let name7 = "kalpesh dhola";
+// console.log(name7.startsWith("dhola")); // check first leter available in alement or not
+
+let name8 = "kalpesh dhola";
+// console.log(name8.endsWith("kalepsh")); // check last leter available in alement or not
+
+let name9 = "kalpesh dhola";
+// console.log(name9.slice(-2)); // give a slice alement with start to end position and also put negative position
+
+let name10 = "kalpesh dhola";
+// console.log(name10.substring(1,5)); // give a slice alement with start to end position
+
+let name11 = "kalpesh dhola";
+// console.log(name11.substr(7,2)); //give a slice alement with start to end position and also put negative position
+
+let name12 = "kalpesh dhola";
+// console.log(name12.toUpperCase()); // give all words in uppercase
+
+let name13 = "kalpesh dhola";
+// console.log(name13.toLowerCase()); // givr all words in lowercase
+
+let name14 = "         kalpesh dhola   ";
+// console.log(name14.trim()); // asjuct spece in allement
+
+let name15 = "kalpesh dhola";
+// console.log(name15.replace("dhola", "ahir")); // replace element with target word to replace word
+
+let name16 = "kalpesh dhola";
+// console.log(name16.repeat(2)); // repeat the full variable
+
+let name17 = "kalpesh";
+let surname = "dhola";
+// console.log(name17.concat(" ",surname)); // join the deffrant alemnet in one element
+
+let name18 = "kalpesh dhola";
+// console.log(name18.split("")); // splite all word leter in element
+
+let name19 = "kalpesh dhola";
+console.log(name19.match("dhsola")); // find and match element in variable 
+
+
+
+
+
+
+
