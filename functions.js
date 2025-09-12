3b8ad17746915b47b2859478b5ba6f55ec131539
kalpesh-dhola <ad.kalpeshdhola@gmail.com>
2025-09-12 12:31:04 +0000

    make some functions in functions.js

--- functions.js
+++ functions.js
@@ -26,8 +26,50 @@ function add(a, b) {
 
 // console.log(add(5, 10)); // Function with Parameters 
 
-function name (name = "who"){
-    console.log("hello" + myname);
+function numberr(number1 , number2){
+  console.log(number1 + number2);
 }
-name();
-name("kalpesh");
+// numberr(20 , 23);
+
+function demo(number1 , number2){
+  let resultt = number1 + number2;
+  return resultt;
+}
+const resultt = demo(20 , 22);
+console.log(resultt);
+
+function demo2(number1 , number2){
+  return number1 + number2;
+}
+const res = demo2(15 , 15);
+console.log(res);
+
+function demo3(number1 , number2){
+  return number1 + number2;
+}
+const ress = demo2(30 ,45);
+console.log(ress);
+
+function demo4(number1 , number2){
+  return number1 + number2;
+}
+const resss = demo4(30 , 45);
+console.log(resss)
+
+function demo5(number1 , number2){
+  return number1 + number2;
+}
+const ressss = demo5(84 , 45);
+console.log(ressss);
+
+function demo6(number1 , number2){
+  return number1 + number2;
+}
+const res6s = demo6(64 , 57);
+console.log(res6s);
+ 
+
+
+
+
+
